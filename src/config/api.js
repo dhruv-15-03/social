5b3e6a4d6e08ee61e1a58220c3283e52a585e557
Dhruv Rastogi <dhruv.rastogi.2004@gmail.com>
2024-09-12 23:28:35 +0530

    backend deployed

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const jwtToken=localStorage.getItem("jwt")
 
-export const API_BASE_URL="http://localhost:8081";
+export const API_BASE_URL="https://social-app-production-1da9.up.railway.app";
 export const api=axios.create({baseURL:API_BASE_URL,headers:{
     "Authorization":`Bearer ${jwtToken}`,
     "Content-Type":"application/json"
